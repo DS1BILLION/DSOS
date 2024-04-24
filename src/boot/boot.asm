32b3c40c44b5861b77b3cc275c61831b234b13b6
Divyanshu sharma <Divyanshusharma469@gmail.com>
2024-04-24 12:49:47 +0530

    loading 32 bit kernel into memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -30,6 +30,7 @@ step2:
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:load32
+    
 
 ;GDT
 gdt_start:
@@ -61,18 +62,61 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
-
 [BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp 
-    jmp $
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read 
+    jmp CODE_SEG: 0x100000
+
+ata_lba_read:
+    mov ebx, eax ;backup LBA
+    shr eax, 24 ;get the sector count
+    or eax, 0xE0 ;select master drive
+    mov dx, 0x1F6   ;drive/head register
+    out dx, al  ;drive/head register
+
+    mov eax, ecx    ;sector count
+    mov dx, 0x1F2   ;sector count
+    out dx, al  ;sector count
+
+    mov eax, ebx   ;LBA
+    mov dx, 0x1F3   ;LBA low
+    out dx, al  ;LBA low
+
+    mov dx, 0x1F4   ;LBA mid
+    mov eax,ebx 
+    shr eax, 8  ;shift 8 bits
+    out dx, al  ;LBA mid
+
+    mov dx, 0x1F5   ;LBA high
+    mov eax, ebx    ;LBA
+    shr eax, 16 ;shift 16 bits
+    out dx, al  ;LBA high
+
+    mov dx, 0x1F7   ;command register
+    mov al, 0x20    ;read command
+    out dx, al  ;command register
+
+    ;read all sector into memory
+.next_sector:
+    push ecx
+
+.try_again:         ;try again if error
+    mov dx, 0x1F7   ;status register
+    in al, dx   ;status register
+    test al, 8   ;check if error
+    jz .try_again   ;try again
+
+    mov ecx, 256    ;sector size
+    mov dx, 0x1F0   ;data register
+    rep insw    ;read sector into memory
+
+    pop ecx
+    loop .next_sector
+
+    ret
 
 
 times 510-($-$$) db 0
